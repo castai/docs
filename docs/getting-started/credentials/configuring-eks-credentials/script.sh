2e1d375472cb7a8f748a014125c5545d5ee323dc
Ernestas Prisakaru <ernestas@cast.ai>
2021-09-02 13:17:40 +0300

    credentials onboarding - aws - send compact credentials instead of pretty printed (#147)

--- docs/getting-started/credentials/configuring-eks-credentials/script.sh
+++ docs/getting-started/credentials/configuring-eks-credentials/script.sh
@@ -84,5 +84,5 @@ if [ -z $CASTAI_API_TOKEN ] || [ -z $CASTAI_API_URL ]; then
   echo "Skipped sending credentials to CAST AI console (CASTAI_API_TOKEN and CASTAI_API_URL variables were not provided)"
 else
   echo "Sending credentials to CAST AI console"
-  curl -fsS -X POST -H "X-API-Key: $CASTAI_API_TOKEN" $CASTAI_API_URL -d "$(jq -n --arg CREDENTIALS "$CREDENTIALS" '{credentials:$CREDENTIALS}')"
+  curl -fsS -X POST -H "X-API-Key: $CASTAI_API_TOKEN" $CASTAI_API_URL -d "$(jq -c -n --arg CREDENTIALS "$CREDENTIALS" '{credentials:$CREDENTIALS}')"
 fi
